0425d20f024ff678836cdf6e4aa64539221734f1
Le Tan <2011436@dlu.edu.vn>
2022-11-30 14:27:51 +0700

    login1

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,12 +2,14 @@ import Product from "../pages/Product";
 import Home from "../pages/Home";
 import Contact from "../pages/Contact";
 import About from "../pages/About";
+import Login from "../pages/Login";
 
 const publicRoutes = [
   { path: "/", component: <Home /> },
   { path: "/about", component: <About /> },
   { path: "/product", component: <Product /> },
   { path: "/contact", component: <Contact /> },
+  { path: "/login", component: <Login /> },
 ];
 
 export { publicRoutes };
