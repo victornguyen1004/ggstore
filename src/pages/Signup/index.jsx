a96c10f276b51801d42372a058ede9bc5adfbe7d
Victor <quanggvuu1004@gmail.com>
2022-12-02 14:10:08 +0700

    addsignup route + back button

---
+++ src/pages/Signup/index.jsx
@@ -0,0 +1,16 @@
+import BackButton from "../../components/BackButton";
+
+function Signup() {
+    return (
+      <div className="h-screen bg-[#f4f6f5] px-28">
+        <div className="flex h-[80px] flex-col py-4">
+          <BackButton />
+        </div>
+        <div className="flex h-[calc(100vh-160px)] w-full items-center min-w-[600px] justify-center">
+          <div className="bg-white w-3/5 h-full">fdsfds</div>
+        </div>
+      </div>
+    );
+}
+
+export default Signup;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,12 +2,14 @@ import Product from "../pages/Product";
 import Home from "../pages/Home";
 import Contact from "../pages/Contact";
 import About from "../pages/About";
+import Signup from "../pages/Signup";
 
 const publicRoutes = [
   { path: "/", component: <Home /> },
   { path: "/about", component: <About /> },
   { path: "/product", component: <Product /> },
   { path: "/contact", component: <Contact /> },
+  { path: "/signup", component: <Signup /> },
 ];
 
 export { publicRoutes };
