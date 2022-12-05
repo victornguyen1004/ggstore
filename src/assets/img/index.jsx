1f6c89b6adb29a2455a8ba470d18a93835cda9c5
Victor <quanggvuu1004@gmail.com>
2022-12-05 13:40:11 +0700

    add signup page

--- src/assets/img/index.jsx
+++ src/assets/img/index.jsx
@@ -1,8 +1,14 @@
 const images = {
-    logoFull: require('./logofull.png'),
-    logo: require('./logo.png'),
-    pochita: require("../img/pochita.png"),
-    banner: require("../img/banner.png"),
+  logoFull: require("./logofull.png"),
+  logo: require("./logo.png"),
+  pochita: require("../img/pochita.png"),
+  banner: require("../img/banner.png"),
+  signupBanner: require("../img/signup.png"),
+  googleMono: require("../img/google_mono.png"),
+  google: require("../img/google.png"),
+  twitterMono: require("../img/twitter_mono.png"),
+  facebook: require("../img/facebook.png"),
+  twitter: require("../img/twitter.png"),
 };
 
 export default images;
