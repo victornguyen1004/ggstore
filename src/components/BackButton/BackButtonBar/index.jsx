a314ec9d43c121697166320e9b4ac1625556deca
Victor <quanggvuu1004@gmail.com>
2022-12-09 23:32:39 +0700

    update developing page

---
+++ src/components/BackButton/BackButtonBar/index.jsx
@@ -0,0 +1,12 @@
+import BackButton from "..";
+
+function BackButtonBar(props) {
+    return ( 
+        <div className="relative flex h-[80px] w-full items-center justify-between py-4">
+            <BackButton />
+            <div className=""></div>
+        </div>
+     );
+}
+
+export default BackButtonBar;
--- src/components/Developing/index.jsx
+++ src/components/Developing/index.jsx
@@ -1,7 +1,27 @@
+import DefaultWrapper from "../Wrappers/Default";
+import BackButtonBar from "../BackButton/BackButtonBar";
+import images from "../../assets/img";
+
 function Developing() {
-    return ( <>
-        
-    </> );
+    return (
+      <>
+        <DefaultWrapper>
+          <BackButtonBar />
+          <div className="flex h-[calc(100vh-80px)] flex-col items-center justify-center">
+            <div className="text-lg font-medium text-zinc-800">
+              New Features Coming Soon!
+            </div>
+            <div className="mt-4">
+              <img
+                src={images.building}
+                alt=""
+                className="w-[320px] object-contain"
+              />
+            </div>
+          </div>
+        </DefaultWrapper>
+      </>
+    );
 }
 
 export default Developing;
